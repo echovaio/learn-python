c9348583a27d01ae4a0dd20929c56dabe75c14d6
lenovok20 <lenovok20@dingdian.mobi>
2017-03-02 17:04:43 +0800

    create ex1-2

---
+++ ex1-2.py
@@ -0,0 +1,12 @@
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+
+vector = Vector(3, 4)
+print(vector)
